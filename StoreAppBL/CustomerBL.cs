ca322f6562bca9c78bdc8f8e9905114adbc5e272
jrm246 <jrm246@att.net>
2021-06-30 15:13:43 -0400

    Added BL for the Customer model

---
+++ StoreAppBL/CustomerBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using StoreModels;
+using StoreAppData;
+
+namespace StoreAppBL
+{
+    public class CustomerBL
+    {
+        public static void AddCustomer(string name, string address, string email, string phone)
+        {
+            Customer custo = new Customer(name, address, email, phone);
+            //CustomerData.AddCustomer(custo);
+        }
+
+        public static Customer SearchCustomer(string name)
+        {
+            // foreach (Customer item in CustomerData.CustomerList())
+            // {
+            //     if (item.Name == name)
+            //     {
+            //         return item;
+            //     }
+            // }
+            // return null;
+            throw new NotImplementedException("Customer Search is not Implemented");
+        }
+    }
+}
