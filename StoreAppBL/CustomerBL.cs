347ab7377cd80410af507919ab05636b004f7fe9
jrm246 <jrm246@att.net>
2021-07-07 17:22:27 -0400

    Added CustomerDL Singleton

--- StoreAppBL/CustomerBL.cs
+++ StoreAppBL/CustomerBL.cs
@@ -10,24 +10,17 @@ namespace StoreAppBL
         public static bool AddCustomer(string name, string address, string email, string phone)
         {
             Customer custo = new Customer(name, address, email, phone);
-            return CustomerDL.AddCustomer(custo);
+            return CustomerDL._customerDL.AddCustomer(custo);
         }
 
         public static Customer SearchCustomer(string name)
         {
-            // foreach (Customer item in CustomerData.CustomerList())
-            // {
-            //     if (item.Name == name)
-            //     {
-            //         return item;
-            //     }
-            // }
-            return CustomerDL.FindCustomer(name);
+            return CustomerDL._customerDL.FindCustomer(name);
         }
 
         public static List<Customer> ListCustomers()
         {
-            return CustomerDL.RetrieveCustomers();
+            return CustomerDL._customerDL.RetrieveCustomers();
         }
     }
 }
--- StoreAppData/CustomerDL.cs
+++ StoreAppData/CustomerDL.cs
@@ -3,22 +3,26 @@ using StoreModels;
 using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
+using StoreAppData.Entities;
 
 namespace StoreAppData
 {
     // Consider changing to Singleton instead of making every function static
-    public class CustomerDL// : ICustomerDL
+    public class CustomerDL : ICustomerDL
     {
         private const string _customerFilePath = "../StoreAppData/StoredData/Customers.json";
-        public static bool AddCustomer(Customer item)
+        // Singleton for CustomerDL
+        public static CustomerDL _customerDL = new CustomerDL();
+        private Entities.JMStoreAppContext _context;
+        public bool AddCustomer(StoreModels.Customer item)
         {
             bool val = false;
             try
             {
                 string customersFile = File.ReadAllText(_customerFilePath);
-                List<Customer> customers = JsonSerializer.Deserialize<List<Customer>>(customersFile);
+                List<StoreModels.Customer> customers = JsonSerializer.Deserialize<List<StoreModels.Customer>>(customersFile);
                 customers.Add(item);
-                JsonSerializer.Serialize<List<Customer>>(customers);
+                JsonSerializer.Serialize<List<StoreModels.Customer>>(customers);
                 string text = JsonSerializer.Serialize(customers, new JsonSerializerOptions(){WriteIndented = true});
                 File.WriteAllText(_customerFilePath, text);
                 val = true;
@@ -30,13 +34,13 @@ namespace StoreAppData
             return val;
         }
 
-        public static Customer FindCustomer(string name)
+        public StoreModels.Customer FindCustomer(string name)
         {
             try
             {
                 string customersFile = File.ReadAllText(_customerFilePath);
-                List<Customer> customers = JsonSerializer.Deserialize<List<Customer>>(customersFile);
-                foreach (Customer item in customers)
+                List<StoreModels.Customer> customers = JsonSerializer.Deserialize<List<StoreModels.Customer>>(customersFile);
+                foreach (StoreModels.Customer item in customers)
                 {
                     if (item.Name == name)
                     {
@@ -51,16 +55,16 @@ namespace StoreAppData
             }
         }
 
-        public static List<Customer> RetrieveCustomers()
+        public List<StoreModels.Customer> RetrieveCustomers()
         {
             try
             {
                 string customersFile = File.ReadAllText(_customerFilePath);
-                return JsonSerializer.Deserialize<List<Customer>>(customersFile);
+                return JsonSerializer.Deserialize<List<StoreModels.Customer>>(customersFile);
             }
             catch (System.Exception)
             {
-                return new List<Customer>();
+                return new List<StoreModels.Customer>();
             }
         }
     }
