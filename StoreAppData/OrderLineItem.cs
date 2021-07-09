fa4e724f9198584c33df5ff09788407c40a8297c
jrm246 <jrm246@att.net>
2021-07-09 16:32:29 -0400

    Bugfix and added method to add orderlineitem to db

--- StoreAppData/OrderLineItem.cs
+++ StoreAppData/OrderLineItem.cs
@@ -7,7 +7,7 @@ namespace StoreAppData
 {
     public class OrderLineItem : Repository, ILineItemDL
     {
-        public static StoreLineItem _orderLineItem = new StoreLineItem(new Entities.JMStoreAppContext(DatabaseConnection.GetDatabaseOptions()));
+        public static OrderLineItem _orderLineItem = new OrderLineItem(new Entities.JMStoreAppContext(DatabaseConnection.GetDatabaseOptions()));
         public OrderLineItem(JMStoreAppContext p_context) : base(p_context)
         {
             _context = p_context;
@@ -41,6 +41,28 @@ namespace StoreAppData
         public bool UpdateLineItem(int id, int addedQuantity)
         {
             throw new System.NotImplementedException();
+        }
+
+        public bool AddLineItem(LineItems lineItem, Entities.Order order)
+        {
+            bool val = false;
+            try
+            {
+                _context.OrderLineItems.Add(
+                    new Entities.OrderLineItem()
+                    {
+                        ProductId = lineItem.Product.Id,
+                        Quantity = lineItem.Count,
+                        Order = order
+                    }
+                );
+                val = true;
+            }
+            catch (System.Exception)
+            {
+                val = false;
+            }
+            return val;
         }
     }
 }
