d77e77372f0f1e7fac480ae3c388d9db37048580
jrm246 <jrm246@att.net>
2021-07-12 18:00:09 -0400

    Business Layer Comments

--- StoreAppBL/CustomerBL.cs
+++ StoreAppBL/CustomerBL.cs
@@ -5,24 +5,49 @@ using System.Collections.Generic;
 
 namespace StoreAppBL
 {
+    /// <summary>
+    /// Responsible for calling any Customer Data Layer methods
+    /// </summary>
     public class CustomerBL
     {
+        /// <summary>
+        /// Creates a new Customer to add to the database
+        /// </summary>
+        /// <param name="name">The name of the new Customer</param>
+        /// <param name="address">The address of the new Customer</param>
+        /// <param name="email">The email of the new Customer</param>
+        /// <param name="phone">The phone number of the new Customer</param>
+        /// <returns>True if the Customer was sucessfully added</returns>
         public static bool AddCustomer(string name, string address, string email, string phone)
         {
             Customer custo = new Customer(name, address, email, phone);
             return CustomerDL._customerDL.AddCustomer(custo);
         }
 
+        /// <summary>
+        /// Calls the data layer to find a customer given the name of the customer
+        /// </summary>
+        /// <param name="name">The name of the Customer</param>
+        /// <returns>The Customer found or null if not found</returns>
         public static Customer SearchCustomer(string name)
         {
             return CustomerDL._customerDL.FindCustomer(name);
         }
 
+        /// <summary>
+        /// Calls the data layer to find a customer given the id of the customer
+        /// </summary>
+        /// <param name="id">The id of the Customer</param>
+        /// <returns>The Customer found or null if not found</returns>
         public static Customer SearchCustomer(int id)
         {
             return CustomerDL._customerDL.FindCustomer(id);
         }
 
+        /// <summary>
+        /// Calls the data layer to retrieve a list of all Customers
+        /// </summary>
+        /// <returns>a list containing every customer in the database</returns>
         public static List<Customer> ListCustomers()
         {
             return CustomerDL._customerDL.RetrieveCustomers();
--- StoreAppBL/OrderBL.cs
+++ StoreAppBL/OrderBL.cs
@@ -7,16 +7,29 @@ namespace StoreAppBL
 {
     public class OrderBL
     {
+        // The Order currently being constructed
         public Orders CurrentOrder { get; set; }
+        
+        // The Store the Order is being ordered from
         public StoreFront CurrentStore { get; set; }
+        
+        // A list of LineItems that contain information of StoreLineItems being ordered
+        // and the quantity of StoreLineItems being ordered
         private List<LineItems> _changedStoreLineItems = new List<LineItems>();
+
+        /// <summary>
+        /// The Constructor for the class
+        /// </summary>
         public OrderBL()
         {
             CurrentOrder = new Orders();
         }
 
-        //public List<Orders> GetOrdersByCustomer() 
-
+        /// <summary>
+        /// Begins the process of making an order.
+        /// </summary>
+        /// <param name="p_customer">The Customer object of the customer making the order</param>
+        /// <param name="p_storeFront">The StoreFront object of the storefront where the order is being ordered from</param>
         public void BeginOrder(Customer p_customer, StoreFront p_storeFront)
         {
             CurrentOrder.LocationId = p_storeFront.Id;
@@ -26,12 +39,22 @@ namespace StoreAppBL
             CurrentStore = p_storeFront;
         }
 
+        /// <summary>
+        /// Creates a LineItem and adds it to the CurrentOrder.
+        /// Checks to make sure that there is enough inventory to order the LineItem.
+        /// </summary>
+        /// <param name="p_id">The Id of the StoreLineItem that the customer wants to order from</param>
+        /// <param name="num">The amount of product the customer wants from the LineItem.
+        /// Cannot be less than 0.</param>
+        /// <returns>True: If the LineItem was successfully created 
+        /// False: If the LineItem could not be made</returns>
         public bool AddOrderItem(int p_id, int num)
         {
             LineItems storeLineItem = StoreLineItem._storeLineItem.FindLineItem(p_id);
             LineItems orderLineItem = null;
             bool val = false;
 
+            // Check to see if product is already being ordered
             foreach (LineItems item in CurrentOrder.LineItems)
             {
                 if (item.Product.Id == storeLineItem.Product.Id)
@@ -39,10 +62,13 @@ namespace StoreAppBL
                     orderLineItem = item;
                 }
             }
+
+            // Check to see if there is enough product to be ordered
             if(num > storeLineItem.Count)
             {
                 val = false;
             }
+            // 
             else if (orderLineItem != null)
             {
                 if (orderLineItem.Count + num > storeLineItem.Count)
