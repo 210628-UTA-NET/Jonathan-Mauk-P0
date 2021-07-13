ac90be2a86a2d9538b956bdab2dba479570839d7
jrm246 <jrm246@att.net>
2021-07-13 17:11:14 -0400

    Added way to browse stores in store inventory menu

--- StoreUI/ViewStoreInv.cs
+++ StoreUI/ViewStoreInv.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using StoreModels;
 using StoreAppBL;
 
@@ -10,7 +11,8 @@ namespace StoreUI
         {
             Console.WriteLine("==== View Store Inventory ====");
             Console.WriteLine("Please choose an option.");
-            Console.WriteLine("[1] View a store's inventory");
+            Console.WriteLine("[2] Browse stores");
+            Console.WriteLine("[1] Choose store by name");
             Console.WriteLine("[0] Go back to Inventory Options");
         }
 
@@ -18,24 +20,28 @@ namespace StoreUI
         {
             string input = Console.ReadLine();
             MenuOptions val = MenuOptions.ViewStoreInv;
+            StoreFront store = null;
             switch (input)
             {
                 case "1":
-                    Console.WriteLine();
                     Console.WriteLine("Please enter the store name you wish to view.");
                     string storename = Console.ReadLine();
-                    StoreFront store = StoreFrontBL._storeFrontBL.FindStore(storename);
+                    store = StoreFrontBL._storeFrontBL.FindStore(storename);
                     if (store != null)
                     {
-                        Console.WriteLine("========================");
-                        Console.WriteLine($"Name: {store.Name}");
-                        Console.WriteLine($"Address: {store.Address}");
-                        Console.WriteLine("Items: ");
-                        foreach (LineItems item in store.Inventory)
-                        {
-                            Console.WriteLine(item.ToString());
-                        }
-                        Console.WriteLine("========================");
+                        ListStoreInventory(store);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Store you searched for could not be found");
+                    }
+                    EnterToContinue();
+                    break;
+                case "2":
+                    store = ChooseStore();
+                    if (store != null)
+                    {
+                        ListStoreInventory(store);
                     }
                     else
                     {
@@ -51,6 +57,40 @@ namespace StoreUI
                     break;
             }
             return val;
+        }
+
+        private StoreFront ChooseStore()
+        {
+            StoreFront store = null;
+            Console.WriteLine("Please choose the store you wish to view.");
+            List<StoreFront> stores = StoreFrontBL._storeFrontBL.RetrieveStores();
+            foreach (StoreFront storeFront in stores)
+            {
+                Console.WriteLine($"[{storeFront.Id}] Name: {storeFront.Name}\t Address: {storeFront.Address}");
+            }
+            string input2 = Console.ReadLine();
+            try
+            {
+                store = StoreFrontBL._storeFrontBL.FindStore(Int32.Parse(input2));
+            }
+            catch (System.Exception)
+            {
+                store = null;
+            }
+            return store;
+        }
+
+        private void ListStoreInventory(StoreFront store)
+        {
+            Console.WriteLine("========================");
+            Console.WriteLine($"Name: {store.Name}");
+            Console.WriteLine($"Address: {store.Address}");
+            Console.WriteLine("Items: ");
+            foreach (LineItems item in store.Inventory)
+            {
+                Console.WriteLine("---- " + item.ToString());
+            }
+            Console.WriteLine("========================");
         }
     }
 }
